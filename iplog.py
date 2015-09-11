f469ea2b4b2c28f4ce640774da95477a5f658e23
ijazahmad722 <ijazahmad722@gmail.com>
2015-09-11 12:55:42 +0500

    iplog.py changed

--- iplog.py
+++ iplog.py
@@ -3,11 +3,11 @@
 with open('/var/log/httpd/access_log' ,'r') as f:
     result = dict()
     for line in f:
-      #  match = re.search(pattern,line)
+        #print line
         line = line.split()
         ip = line[0]
-       # print ip
-        if '200' in line:
-            result[ip]=result.setdefault(ip,0)+1
+        print ip
+       # if '200' in line:
+        result[ip]=result.setdefault(ip,0)+1
     for ip , count in sorted(result.items(), key = lambda x: x[1], reverse=True)[1:10]:
        print ip , count        
